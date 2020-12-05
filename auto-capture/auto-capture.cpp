16375296d880c9355de005bcf81d3b2ec2254629
mamil <mamilgithub@gmail.com>
2020-12-05 20:29:13 +0800

    add example

--- auto-capture/auto-capture.cpp
+++ auto-capture/auto-capture.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <sstream>
 #include "Capture.h"
 
 int main()
 {
-    std::cout << "statr auto-capture!\n";
+    std::cout << "statr auto-capture!" << std::endl;
+    std::cout << "get interface:" << std::endl;
     Capture aCapture;
-    aCapture.setConfig("-D");
-    aCapture.startCapture();
+    aCapture.getInterfaceInfo();
+
+    std::cout << "capture interface no: (-1 to input command directly)" << std::endl;
+    int no = 0;
+    std::cin >> no;
+
+    if (no == -1)
+    {
+        std::cout << "please input command:" << std::endl;
+        std::string command = "";
+        std::cin >> command;
+        aCapture.setConfig(command);
+        aCapture.startCapture();
+    }
+    else
+    {
+        std::ostringstream ostr;
+        ostr << "-i " << no << " -s 0 -C 50 -W 100 -w .\\80.pcap port 80 or 8080 or 443";
+
+        std::string command = ostr.str();
+        aCapture.setConfig(command);
+        aCapture.startCapture();
+    }
+
+    system("pause");
 }
